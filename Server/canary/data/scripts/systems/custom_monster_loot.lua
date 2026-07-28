ecbc61729b619dd9fb57dad0f850bfe9b7d698a6
YaanMark <yandumarques@gmail.com>
2026-07-28 10:45:28 -0300

    fix(server): remove global christmas token drop from custom_monster_loot.lua

--- Server/canary/data/scripts/systems/custom_monster_loot.lua
+++ Server/canary/data/scripts/systems/custom_monster_loot.lua
@@ -1,6 +1,5 @@
--- Drops custom loot for all monsters
 local allLootConfig = {
-	{ id = 6526, chance = 100000, minCount = 1, maxCount = 10 }, -- Example of loot (100% chance)
+	-- Tabela vazia para desativar o drop global de christmas tokens
 }
 
 -- Custom loot for specific monsters (this has the same usage options as normal monster loot)
